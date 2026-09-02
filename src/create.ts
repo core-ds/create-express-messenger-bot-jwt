0db15542f7db8a3e6d2efd86b7eabd80f4c622ee
hextion <100ishundred@gmail.com>
2026-09-02 18:54:55 +0300

    Initial commit

---
+++ src/create.ts
@@ -0,0 +1,28 @@
+import * as core from "@actions/core";
+import jwt from "jsonwebtoken";
+import { randomUUID } from "node:crypto";
+
+export function main(): string {
+  const serverUrl = core.getInput("express-server-url", { required: true });
+  const botId = core.getInput("bot-id", { required: true });
+  const secretKey = core.getInput("secret-key", { required: true });
+  const expiresIn = Number(core.getInput("expires-in") || 10);
+  const nowInSeconds = Math.floor(Date.now() / 1000);
+
+  const payload = {
+    iss: botId,
+    aud: URL.canParse(serverUrl) ? URL.parse(serverUrl)!.host : serverUrl,
+    exp: nowInSeconds + 60 * expiresIn,
+    nbf: nowInSeconds,
+    iat: nowInSeconds,
+    jti: randomUUID(),
+    version: 2,
+  };
+
+  const token = jwt.sign(payload, secretKey, { algorithm: "HS256" });
+
+  core.setSecret(token);
+  core.setOutput("token", token);
+
+  return token;
+}
